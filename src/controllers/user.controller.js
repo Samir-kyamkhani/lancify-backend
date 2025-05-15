65d4563df649e0742af57d583ee40c4c94b99c78
Samir khan <samirkayamkhani.dev@gmail.com>
2025-05-15 20:44:18 +0530

    fix login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -154,12 +154,12 @@ const login = asyncHandler(async (req, res) => {
       where: { email: googleUser.email },
     });
 
-    await prisma.user.update({
+    const updatedUser = await prisma.user.update({
       where: { id: user.id },
       data: { isGoogleSignUp: true },
     });
 
-    if (!user || !user.isGoogleSignUp) {
+    if (!updatedUser || !updatedUser.isGoogleSignUp) {
       return ApiError.send(
         res,
         404,
