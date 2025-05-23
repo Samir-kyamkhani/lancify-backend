06972c5e33b023fbe6966c6e0c156cd3d014733d
Samir khan <samirkayamkhani.dev@gmail.com>
2025-05-23 15:17:04 +0530

    tasks routes added

--- src/routes/tasksRouter.routes.js
+++ src/routes/tasksRouter.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { createTask } from "../controllers/Taskboard.controller.js";
+import { createTask, deleteTask, getAllTasks, getTaskById, updateTask } from "../controllers/Taskboard.controller.js";
 
 const router = Router();
 
 router.post("/create-task", authMiddleware, createTask);
-// router.put("/update-proposal/:id", authMiddleware, updateProposal);
-// router.get("/get-all-proposals", authMiddleware, getAllProposals);
-// router.get("/get-proposal/:id", authMiddleware, getProposalById);
-// router.delete("/delete-proposal/:id", authMiddleware, deleteProposal);
+router.put("/update-task/:id", authMiddleware, updateTask);
+router.get("/get-all-tasks", authMiddleware, getAllTasks);
+router.get("/get-task/:id", authMiddleware, getTaskById);
+router.delete("/delete-task/:id", authMiddleware, deleteTask);
 
 export default router;
